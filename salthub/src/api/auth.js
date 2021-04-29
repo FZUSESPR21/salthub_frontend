e1aedf5cf11c59e92b9b91e98ca1e4330df745db
lvvvv <375042084@qq.com>
2021-04-29 19:47:10 +0800

    fix: 注释了部分输出，修改了登录接口url的参数

--- salthub/src/api/auth.js
+++ salthub/src/api/auth.js
@@ -1,15 +1,21 @@
 import request from '@/utils/request'
 
 export function login(data) {
+    const one = '1' //暂时代替url中记住密码的参数
     return request({
-        url: '/account/login/1',
+        // url: '/account/login/1',
+        url: `/account/login/${data.status}`,
         method: 'post',
         data
     })
 }
 
-export function getUserInfo() {
-
+export function register(data) {
+    return request({
+        url: '/account/login/1',
+        method: 'post',
+        data
+    })
 }
 
 export function logout() {
