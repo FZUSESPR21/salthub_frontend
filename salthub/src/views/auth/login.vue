c66cdcf8c91cd0f871905b22d48c8fa3b850a423
lvvvv <375042084@qq.com>
2021-04-30 20:35:00 +0800

    feat: 登录接口完成

--- salthub/src/views/auth/login.vue
+++ salthub/src/views/auth/login.vue
@@ -1,45 +1,54 @@
 <template>
-<div class="login-container">
+  <div class="login-container">
     <el-card shadow="never" class="login-card">
-        <div class="header" align="center">
-        <img alt="register logo"  src="../../assets/ForumLogo.png">
-        </div>
-        <div>
-          <el-form
-            v-loading="loading"
-            :model="ruleForm"
-            status-icon
-            :rules="rules"
-            ref="ruleForm"
-            label-width="100px"
-            class="demo-ruleForm"
-          >
-            <el-form-item label="账号" prop="name">
-              <el-input v-model="ruleForm.name"></el-input>
-            </el-form-item>
+      <div class="header" align="center">
+        <img alt="register logo" src="../../assets/ForumLogo.png" />
+      </div>
+      <div>
+        <el-form
+          v-loading="loading"
+          :model="ruleForm"
+          status-icon
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="账号" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
 
-            <el-form-item label="密码" prop="pass">
-              <el-input
-                type="password"
-                v-model="ruleForm.pass"
-                autocomplete="off"
-              ></el-input>
-            </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+              type="password"
+              v-model="ruleForm.password"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
 
-            <el-form-item label="记住" prop="delivery">
-              <el-switch v-model="ruleForm.rememberMe"></el-switch>
-            </el-form-item>
+          <el-form-item label="记住" prop="status">
+            <el-switch
+              v-model="ruleForm.status"
+              :active-value="1"
+              :inactive-value="0"
+            >
+            </el-switch>
+          </el-form-item>
 
-            <el-form-item>
-              <el-button class="tijiao" type="primary" @click="submitForm('ruleForm')"
-                >提交</el-button
-              >
-              <el-button @click="resetForm('ruleForm')">注册</el-button>
-            </el-form-item>
-          </el-form>
-        </div>
+          <el-form-item>
+            <el-button
+              class="submit"
+              type="primary"
+              @click="submitForm('ruleForm')"
+              >提交</el-button
+            >
+            <!-- 注册待跳转 -->
+            <el-button @click="toRegister()">注册</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </el-card>
-</div>
+  </div>
 </template>
 
 <script>
@@ -51,20 +60,20 @@ export default {
       loading: false,
       ruleForm: {
         name: "",
-        pass: "",
-        rememberMe: true,
+        password: "",
+        status: true,
       },
       rules: {
         name: [
           { required: true, message: "请输入账号", trigger: "blur" },
           {
-            min: 2,
+            min: 6,
             max: 15,
-            message: "长度在 2 到 15 个字符",
+            message: "长度在 6 到 15 个字符",
             trigger: "blur",
           },
         ],
-        pass: [
+        password: [
           { required: true, message: "请输入密码", trigger: "blur" },
           {
             min: 6,
@@ -77,32 +86,59 @@ export default {
     };
   },
   methods: {
-  }
-}  
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          // console.log(this.ruleForm)
+          this.$store
+            .dispatch("user/login", this.ruleForm)
+            .then(() => {
+              this.$message({
+                message: "恭喜你，登录成功",
+                type: "success",
+                duration: 2000,
+              });
+              setTimeout(() => {
+                this.loading = false;
+                this.$router.push({ path: this.redirect || "/home" });
+              }, 0.1 * 1000);
+            })
+            .catch(() => {
+              this.loading = false;
+            });
+        } else {
+          return false;
+        }
+      });
+    },
+    toRegister() {
+      this.$router.push({ path: this.redirect || "/register" });
+    },
+  },
+};
 </script>
 
 <style scoped>
-.el-button.tijiao{
-   margin-left: 20px;
+.el-button.submit {
+  margin-left: 20px;
 }
-.el-button{
+.el-button {
   margin-left: 100px;
 }
 
-.login-container{
-    background-image: url("../../assets/study3.jpeg");
-    background-size: 100% 100%;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.login-container {
+  background-image: url("../../assets/study3.jpeg");
+  background-size: 100% 100%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .login-card {
-    width: 530px;
-    height: 500px;
+  width: 530px;
+  height: 500px;
 }
-
-
 </style>
