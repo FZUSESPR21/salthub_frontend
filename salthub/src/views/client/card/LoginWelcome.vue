de5a12af14543c1cae319f9751184d3b4182613a
hannah-shaw <xiaohannwork@163.com>
2021-05-01 17:07:30 +0800

    feat:新增侧边栏路由及js实现倒计时

--- salthub/src/views/client/card/LoginWelcome.vue
+++ salthub/src/views/client/card/LoginWelcome.vue
@@ -2,10 +2,10 @@
  * @description: 晒研论坛-侧边栏-发帖
  * @fileName: LoginWelcome.vue 
  * @author: 肖寒 
- * @date: 2021-04-29 23:09:28 
+ * @date: 2021-05-01 17:09:29 
  * @后台人员:  
  * @path:  
- * @version: V1.0.0 
+ * @version: V1.0.1 
 !-->
 <template>
   <el-card class="box-card" shadow="never">
@@ -16,13 +16,13 @@
     <!--判定是否有token，有则判断登陆成功允许发布帖子-->
     <!--
     <div v-if="token != null && token !== ''" class="has-text-centered">
-      <el-button type="primary" plain tag="router-link" :to="{path:'/'}" outlined>✍ 发表想法</el-button>
+      <el-button type="primary" plain @click="toUserCreatePost()" outlined>✍ 发表想法</el-button>
     </div>
     !-->
     <!--否则显示登陆注册-->
     <!--<div v-else class="has-text-centered">-->
-      <el-button type="primary" plain tag="router-link" :to="{path:'/'}" outlined>马上入驻</el-button>
-      <el-button type="warning" plain tag="router-link" :to="{path:'/'}" outlined class="ml-2"> 论坛登录</el-button>
+      <el-button type="primary" plain @click="toRegister()" outlined>马上入驻</el-button>
+      <el-button type="warning" plain @click="toLogin()" outlined class="ml-2"> 论坛登录</el-button>
     <!--</div>-->
   </el-card>
 </template>
@@ -40,6 +40,38 @@ export default {
   data() {
     return {}
   },
+  methods:{
+    /**
+     *@functionName:    toRegister 
+     *@description: 跳转注册页面
+     *@author: lw
+     *@date: 2021-05-01 14:59:52
+     *@version: V1.0.0
+    */
+    toRegister() {
+      this.$router.push({ path: this.redirect || "/register" });
+    },
+    /**
+     *@functionName:    toLogin 
+     *@description: 跳转登录页面
+     *@author: xiaohan
+     *@date: 2021-05-01 17:00:03
+     *@version: V1.0.0
+    */
+    toLogin() {
+      this.$router.push({ path: this.redirect || "/login" });
+    },
+    /**
+     *@functionName:    toUserCreatePost 
+     *@description: 跳转发布文章界面
+     *@author: xaiohan
+     *@date: 2021-05-01 17:04:25
+     *@version: V1.0.0
+    */
+    toUserCreatePost() {
+      this.$router.push({ path: this.redirect || "/UserCreatePost" });
+    },
+  }
 }
 </script>
 
