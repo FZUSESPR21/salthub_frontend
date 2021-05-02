9e28b7054be3eaabafd46dc4f1d888a6bf5b74c1
lvvvv <375042084@qq.com>
2021-05-02 23:03:45 +0800

    style: 修改缩进格式

--- salthub/src/views/auth/login.vue
+++ salthub/src/views/auth/login.vue
@@ -15,12 +15,12 @@
           class="demo-ruleForm"
         >
           <el-form-item label="账号" prop="name">
-            <el-input style="width:85%" v-model="ruleForm.name"></el-input>
+            <el-input style="width: 85%" v-model="ruleForm.name"></el-input>
           </el-form-item>
 
           <el-form-item label="密码" prop="password">
             <el-input
-              style="width:85%"
+              style="width: 85%"
               type="password"
               v-model="ruleForm.password"
               autocomplete="off"
@@ -37,13 +37,15 @@
           </el-form-item>
 
           <transition name="fade">
-            <SliderCheck v-show="sliderShown" class="formItem" :successFun="successVerified" :errorFun="errorVerified"></SliderCheck>
+            <SliderCheck
+              v-show="sliderShown"
+              class="formItem"
+              :successFun="successVerified"
+              :errorFun="errorVerified"
+            ></SliderCheck>
           </transition>
           <el-form-item>
-            <el-button
-              class="submit"
-              type="primary"
-              @click="submit('ruleForm')"
+            <el-button class="submit" type="primary" @click="submit('ruleForm')"
               >登录</el-button
             >
             <!-- 注册待跳转 -->
@@ -56,17 +58,17 @@
 </template>
 
 <script>
-import SliderCheck from '@/components/auth/SliderCheck'
+import SliderCheck from "@/components/auth/SliderCheck";
 export default {
   name: "login",
-  components:{ 
+  components: {
     SliderCheck,
   },
   data() {
     return {
       redirect: undefined,
       loading: false,
-      isVerified: false,  //是否通过人机验证
+      isVerified: false, //是否通过人机验证
       sliderShown: false, //显示人机验证滑块
       ruleForm: {
         name: "",
@@ -97,13 +99,13 @@ export default {
   },
   methods: {
     /**
-     *@functionName:    submitForm 
+     *@functionName:    submitForm
      *@params: formName 表单名称
      *@description: 提交表单进行登录验证
      *@author: lw
      *@date: 2021-05-01 14:58:59
      *@version: V1.0.0
-    */
+     */
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
@@ -131,52 +133,52 @@ export default {
       });
     },
     /**
-     *@functionName:    toRegister 
+     *@functionName:    toRegister
      *@description: 跳转注册页面
      *@author: lw
      *@date: 2021-05-01 14:59:52
      *@version: V1.0.0
-    */
+     */
     toRegister() {
       this.$router.push({ path: this.redirect || "/register" });
     },
     /**
-     *@functionName:    显示slider 
+     *@functionName:    显示slider
      *@description: 点击登录 显示slider进行人机验证
      *@author: lw
      *@date: 2021-05-02 17:50:39
      *@version: V1.0.0
-    */
+     */
     submit(form) {
-      this.sliderShown = true
-      console.log(this.isVerified)
+      this.sliderShown = true;
+      console.log(this.isVerified);
       if (this.isVerified == true) {
-        this.submitForm(form)
+        this.submitForm(form);
       }
     },
     /**
-     *@functionName:    successVerified 
+     *@functionName:    successVerified
      *@description: 滑块验证成功函数
      *@author: lw
      *@date: 2021-05-02 20:18:19
      *@version: V1.0.0
-    */
+     */
     successVerified() {
       // console.log("成功验证")
-      this.isVerified = true
+      this.isVerified = true;
       // this.submitForm(this.ruleForm)
     },
     /**
-     *@functionName:    errorVerified 
+     *@functionName:    errorVerified
      *@description: 滑块验证失败函数
      *@author: lw
      *@date: 2021-05-02 20:18:19
      *@version: V1.0.0
-    */
+     */
     errorVerified() {
       // console.log("验证失败")
-      this.isVerified = false 
-    }
+      this.isVerified = false;
+    },
   },
 };
 </script>
@@ -217,6 +219,6 @@ export default {
   transition: opacity 1s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active, 2.1.8 版本以下 */ {
-    opacity: 0
+  opacity: 0;
 }
 </style>
--- salthub/src/views/auth/register.vue
+++ salthub/src/views/auth/register.vue
@@ -1,145 +1,164 @@
 <template>
   <div class="register-container">
-      <el-card shadow="never" class="register-card">
-        <div class="header" align="center">
-        <img alt="register logo"  src="../../assets/ForumLogo.png">
-        </div>
-        <div>
-          <el-form
-            ref="ruleForm"
-            v-loading="loading"
-            :model="ruleForm"
-            status-icon
-            :rules="rules"
-            label-width="100px"
-            class="demo-ruleForm"
-          >
-            <el-form-item label="账号" prop="name">
-              <el-input style="width:85%" v-model="ruleForm.name" />
-            </el-form-item>
+    <el-card shadow="never" class="register-card">
+      <div class="header" align="center">
+        <img alt="register logo" src="../../assets/ForumLogo.png" />
+      </div>
+      <div>
+        <el-form
+          ref="ruleForm"
+          v-loading="loading"
+          :model="ruleForm"
+          status-icon
+          :rules="rules"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="账号" prop="name">
+            <el-input style="width: 85%" v-model="ruleForm.name" />
+          </el-form-item>
 
-            <el-form-item label="密码" prop="password">
-              <el-input
-                style="width:85%"
-                v-model="ruleForm.password"
-                type="password"
-                autocomplete="off"
-              />
-            </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input
+              style="width: 85%"
+              v-model="ruleForm.password"
+              type="password"
+              autocomplete="off"
+            />
+          </el-form-item>
 
-            <el-form-item label="确认密码" prop="checkPass">
-              <el-input
-                style="width:85%"
-                v-model="ruleForm.checkPass"
-                type="password"
-                autocomplete="off"
-              />
-            </el-form-item>
+          <el-form-item label="确认密码" prop="checkPass">
+            <el-input
+              style="width: 85%"
+              v-model="ruleForm.checkPass"
+              type="password"
+              autocomplete="off"
+            />
+          </el-form-item>
 
-            
-            <el-form-item label="邮箱" prop="mailbox">
-              <el-input style="width:55%" ref="mailboxAdd" v-model="ruleForm.mailbox" autocomplete="off" />          
-              <el-button v-if="!waitMailCode" class="code" type="primary" @click="getCode()">获取验证码</el-button>
-              <el-button v-if="waitMailCode" class="code" type="primary" disabled>{{waitTime}} 秒后重新获取</el-button>
-            </el-form-item>
-            <transition name="fade">
-              <SliderCheck v-show="sliderShown" class="formItem" :successFun="successVerified" :errorFun="errorVerified"></SliderCheck>
-            </transition>
-           
-            <el-form-item label="验证码">
-              <el-input style="width:55%" v-model="ruleForm.code" />
-            </el-form-item>
+          <el-form-item label="邮箱" prop="mailbox">
+            <el-input
+              style="width: 55%"
+              ref="mailboxAdd"
+              v-model="ruleForm.mailbox"
+              autocomplete="off"
+            />
+            <el-button
+              v-if="!waitMailCode"
+              class="code"
+              type="primary"
+              @click="getCode()"
+              >获取验证码</el-button
+            >
+            <el-button v-if="waitMailCode" class="code" type="primary" disabled
+              >{{ waitTime }} 秒后重新获取</el-button
+            >
+          </el-form-item>
+          <transition name="fade">
+            <SliderCheck
+              v-show="sliderShown"
+              class="formItem"
+              :successFun="successVerified"
+              :errorFun="errorVerified"
+            ></SliderCheck>
+          </transition>
 
-            <el-form-item>
-              <el-button
-                class="register-button"
-                type="primary"
-                style="margin-left:20px"
-                @click="submitForm('ruleForm')">注册</el-button>
-              <el-button 
-                style="margin-left:100px" 
-                @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-          </el-form>
-        </div>
-      </el-card>
+          <el-form-item label="验证码">
+            <el-input style="width: 55%" v-model="ruleForm.code" />
+          </el-form-item>
+
+          <el-form-item>
+            <el-button
+              class="register-button"
+              type="primary"
+              style="margin-left: 20px"
+              @click="submitForm('ruleForm')"
+              >注册</el-button
+            >
+            <el-button style="margin-left: 100px" @click="resetForm('ruleForm')"
+              >重置</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
-import { register,getMailCode } from '@/api/auth'
-import SliderCheck from '@/components/auth/SliderCheck'
+import { register, getMailCode } from "@/api/auth";
+import SliderCheck from "@/components/auth/SliderCheck";
 
 export default {
-  name: 'Register',
+  name: "Register",
   components: {
     SliderCheck,
   },
   data() {
     const validatePass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请再次输入密码'))
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
       } else if (value !== this.ruleForm.password) {
-        callback(new Error('两次输入密码不一致!'))
+        callback(new Error("两次输入密码不一致!"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     return {
       loading: false,
       waitMailCode: false,
-      isVerified: false,  //是否通过人机验证
+      isVerified: false, //是否通过人机验证
       sliderShown: false, //显示人机验证滑块
       waitTime: 6,
       ruleForm: {
-        name: '',
-        password: '',
-        checkPass: '',
-        mailbox:'',
-        code:''
+        name: "",
+        password: "",
+        checkPass: "",
+        mailbox: "",
+        code: "",
       },
       rules: {
         name: [
-          { required: true, message: '请输入账号', trigger: 'blur' },
+          { required: true, message: "请输入账号", trigger: "blur" },
           {
             min: 2,
             max: 10,
-            message: '长度在 2 到 10 个字符',
-            trigger: 'blur'
-          }
+            message: "长度在 2 到 10 个字符",
+            trigger: "blur",
+          },
         ],
         password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
+          { required: true, message: "请输入密码", trigger: "blur" },
           {
             min: 6,
             max: 20,
-            message: '长度在 6 到 20 个字符',
-            trigger: 'blur'
-          }
+            message: "长度在 6 到 20 个字符",
+            trigger: "blur",
+          },
         ],
         checkPass: [
-          { required: true, message: '请再次输入密码', trigger: 'blur' },
-          { validator: validatePass, trigger: 'blur' }
+          { required: true, message: "请再次输入密码", trigger: "blur" },
+          { validator: validatePass, trigger: "blur" },
         ],
         mailbox: [
-          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
           {
-            type: 'email',
-            message: '请输入正确的邮箱地址',
-            trigger: ['blur', 'change']
-          }
-        ]
-      }
-    }
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"],
+          },
+        ],
+      },
+    };
   },
   methods: {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          this.loading = true
+          this.loading = true;
           var code = {
             code: this.ruleForm.code,
-          }
+          };
           var data = {
             // avatar: "",
             email: this.ruleForm.mailbox,
@@ -147,114 +166,113 @@ export default {
             // nickname: "",
             password: this.ruleForm.password,
             // status: "",
-          }
+          };
           register(code, data)
             .then((value) => {
               // console.log(value)
-              const { code, message } = value.data
+              const { code, message } = value.data;
               if (code === 200) {
                 this.$message({
-                  message: '账号注册成功',
-                  type: 'success'
-                })
+                  message: "账号注册成功",
+                  type: "success",
+                });
                 setTimeout(() => {
-                  this.loading = false
-                  this.$router.push({ path: this.redirect || '/login' })
-                }, 0.1 * 1000)
+                  this.loading = false;
+                  this.$router.push({ path: this.redirect || "/login" });
+                }, 0.1 * 1000);
               } else {
                 // console.log(code)
-                this.$message.error('注册失败，' + message)
+                this.$message.error("注册失败，" + message);
               }
             })
             .catch(() => {
-              this.loading = false
-            })
+              this.loading = false;
+            });
         } else {
-          return false
+          return false;
         }
-      })
+      });
     },
     resetForm(formName) {
-      this.$refs[formName].resetFields()
+      this.$refs[formName].resetFields();
     },
     /**
-     *@functionName: getCode 
+     *@functionName: getCode
      *@description: 通过人机验证后，获取邮箱验证码
      *@author: lw
      *@date: 2021-05-02 20:00:18
      *@version: V1.0.1
-    */
+     */
     getCode() {
       var verify = /^\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/;
-      console.log(this.waitMailCode)
+      console.log(this.waitMailCode);
       this.sliderShown = true;
-      if (verify.test(this.ruleForm.mailbox) && this.isVerified) { //通过邮箱格式验证且通过人机验证
+      if (verify.test(this.ruleForm.mailbox) && this.isVerified) {
+        //通过邮箱格式验证且通过人机验证
         // console.log('验证成功')
-        this.waitTime = 60,
-        this.waitMailCode = true 
+        (this.waitTime = 60), (this.waitMailCode = true);
         var timer = setInterval(() => {
-          this.waitTime--
+          this.waitTime--;
           if (this.waitTime <= 0) {
-            this.waitMailCode = false
-            clearInterval(timer) 
+            this.waitMailCode = false;
+            clearInterval(timer);
           }
-        },1000)
+        }, 1000);
         var email = {
-          email: this.ruleForm.mailbox
-        }
+          email: this.ruleForm.mailbox,
+        };
         getMailCode(email)
           .then((response) => {
-            console.log(response)
+            console.log(response);
           })
-          .catch(() => {})
-      }
-      else {
-      console.log(this.waitMailCode)
-        console.log('验证失败')
+          .catch(() => {});
+      } else {
+        console.log(this.waitMailCode);
+        console.log("验证失败");
       }
     },
 
     /**
-     *@functionName:    successVerified 
+     *@functionName:    successVerified
      *@description: 滑块验证成功函数
      *@author: lw
      *@date: 2021-05-02 20:18:19
      *@version: V1.0.0
-    */
+     */
     successVerified() {
-      console.log("成功验证")
-      this.isVerified = true
+      console.log("成功验证");
+      this.isVerified = true;
       // this.submitForm(this.ruleForm)
     },
     /**
-     *@functionName:    errorVerified 
+     *@functionName:    errorVerified
      *@description: 滑块验证失败函数
      *@author: lw
      *@date: 2021-05-02 20:18:19
      *@version: V1.0.0
-    */
+     */
     errorVerified() {
       // console.log("验证失败")
-      this.isVerified = false 
-    }
-  }
-}
+      this.isVerified = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
-.register-container{
-    background-image: url("../../assets/study3.jpeg");
-    background-size: 100% 100%;
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.register-container {
+  background-image: url("../../assets/study3.jpeg");
+  background-size: 100% 100%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .register-card {
-    width: 530px;
-    height: 600px;
+  width: 530px;
+  height: 600px;
 }
 
 .formItem {
@@ -270,7 +288,6 @@ export default {
   transition: opacity 1s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active, 2.1.8 版本以下 */ {
-    opacity: 0
+  opacity: 0;
 }
-
 </style>
