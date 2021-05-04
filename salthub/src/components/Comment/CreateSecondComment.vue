3690f91e650f89b8be9346a2aa61669c1639d870
fzu221801127 <1647688598@qq.com>
2021-05-04 14:54:07 +0800

    feat: 二级评论表单(未完善隐藏显示功能)

---
+++ salthub/src/components/Comment/CreateSecondComment.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="box comments" style="back-groudcolor:#f3f3f3">
+    <h3 class="title is-5">评论</h3>
+
+    <lv-comments-form :slug="slug" @loadComments="fetchComments"/>
+
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { fetchCommentsByTopicId } from '@/api/comment'
+import LvCommentsForm from './SecondCommentsForm'
+
+export default {
+  name: 'LvComments',
+  components: {
+    LvCommentsForm,
+  },
+  data() {
+    return {
+      comments: []
+    }
+  },
+  props: {
+    slug: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'token'
+    ])
+  },
+  async mounted() {
+    // await this.fetchComments(this.slug)
+  },
+  methods: {
+    // 初始化
+    // async fetchComments(topic_id) {
+    //   console.log(topic_id)
+    //   fetchCommentsByTopicId(topic_id).then(response => {
+    //     const { data } = response
+    //     this.comments = data
+    //   })
+    // }
+  }
+}
+</script>
