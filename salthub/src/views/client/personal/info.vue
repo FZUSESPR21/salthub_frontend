6a2410d4a3decb3b1cc55b3838fbdd8db61c0a44
NoMornings <80037319+NoMornings@users.noreply.github.com>
2021-05-06 23:38:40 +0800

    feat: 新增个人信息静态页面 | fixed #30

---
+++ salthub/src/views/client/personal/info.vue
@@ -0,0 +1,67 @@
+<!-- 
+ * @description: 晒研论坛-个人信息
+ * @fileName: info.vue 
+ * @author: NoMornings 
+ * @date: 2021-05-6 21:42:17 
+ * @后台人员:  
+ * @path:  /info
+ * @version: V1.0.0 
+!-->
+<template>
+  <div>
+    <div class="front-head" v-if="refresh">
+      <Header></Header>
+    </div>
+    <div class="front-main">
+      <div class="container context">
+        <div class="columns">
+          <div class="column is-9 is-offset-1">
+            <v-personal></v-personal>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.front-head {
+  margin-bottom: 20px;
+}
+</style>
+
+<script>
+import Header from "@/components/Layout/Header";
+import Footer from "@/components/Layout/Footer";
+import card from "@/components/personal/card";
+import "@/assets/app.css";
+import "buefy/dist/buefy.css";
+
+export default {
+  name: "Home",
+  components: { Header, Footer, "v-personal": card },
+  data() {
+    return {
+      refresh: true,
+    };
+  },
+  created() {
+    this.refreshComp();
+  },
+  methods: {
+    //解决vue页头懒加载导致组件错位的问题
+    refreshComp() {
+      // 移除组件
+      this.refresh = false;
+      // 在组件移除后，重新渲染组件
+      // this.$nextTick可实现在DOM 状态更新后，执行传入的方法。
+      this.$nextTick(() => {
+        this.refresh = true;
+      });
+    },
+  },
+};
+</script>
