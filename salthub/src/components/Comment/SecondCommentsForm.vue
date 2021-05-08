db79e56a59738e96abf3810334675aa1e2056add
fzu221801127 <1647688598@qq.com>
2021-05-08 22:32:16 +0800

    feat: 二级评论接口(还有一个参数author是写死的)

--- salthub/src/components/Comment/SecondCommentsForm.vue
+++ salthub/src/components/Comment/SecondCommentsForm.vue
@@ -14,6 +14,7 @@
           <el-button
             type="primary"
             round
+            native-type="submit"
             :disabled="isLoading"
           >
             发送
@@ -26,7 +27,7 @@
 
 <script>
 import { pushSecondComment } from '@/api/comment'
-
+import store from '@/store'
 export default {
   name: 'LvCommentsForm',
   data() {
@@ -40,27 +41,57 @@ export default {
     slug: {
       type: String,
       default: null
+    },
+    firstCommentId: {
+      type: Number,
+      default: 1
+    },
+    firstCommentAuthor: {
+      type: String,
+      default: null
     }
   },
   methods: {
     async onSubmit() {
-      // this.isLoading = true
-      // try {
-      //   let postData = {}
-      //   console.log(this.commentText)
-      //   postData['content'] = this.commentText
-      //   postData['topic_id'] = this.slug
-      //   await pushComment(postData)
-      //   this.$emit('loadComments', this.slug)
-      //   this.$message.success('留言成功')
-      // } catch (e) {
-      //   this.$buefy.toast.open({
-      //     message: `Cannot comment this story. ${e}`,
-      //     type: 'is-danger'
-      //   })
-      // } finally {
-      //   this.isLoading = false
-      // }
+      this.isLoading = true
+      try {
+        console.log(this.firstCommentId)
+        console.log(this.firstCommentAuthor)
+        // var data = {
+        //   "accountName": this.firstCommentAuthor,
+        //   "author": store.getters.user,
+        //   "commentId": this.firstCommentId,
+        //   "content": this.textarea,
+        //   "id": 1,
+        //   "releaseTime": ""
+        // }
+        var data = {
+          "accountName": this.firstCommentAuthor,
+          "author": "zhuangweilong",
+          "commentId": this.firstCommentId,
+          "content": this.textarea,
+          "id": 1,
+          "releaseTime": ""
+        }
+        console.log(store.getters.user+"######")
+        pushSecondComment(data)
+        this.$message.success('留言成功')
+        this.textarea = ''
+        // let postData = {}
+        // console.log(this.commentText)
+        // postData['content'] = this.commentText
+        // postData['topic_id'] = this.slug
+        // await pushComment(postData)
+        // this.$emit('loadComments', this.slug)
+        // this.$message.success('留言成功')
+      } catch (e) {
+        this.$buefy.toast.open({
+          message: `Cannot comment this story. ${e}`,
+          type: 'is-danger'
+        })
+      } finally {
+        this.isLoading = false
+      }
     }
   }
 }
