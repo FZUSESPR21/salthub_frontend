c611ea85dcf970591f2f47e0c3fdce339ea67808
lvvvv <375042084@qq.com>
2021-05-10 19:46:21 +0800

    feat: 设置一天的localstorage过期时间

--- salthub/src/utils/auth.js
+++ salthub/src/utils/auth.js
@@ -3,13 +3,16 @@ const uToken = 'u_token'
 // 获取Token
 export function getToken() {
     // return Cookies.get(uToken);
-    return localStorage.getItem('jwt')
+    // return localStorage.getItem('jwt')
+    return myLocalStorage.get('jwt')
 }
 
 // 设置Token，1天,与后端同步
 export function setToken(token) {
     // return Cookies.set(uToken, token, {expires: 1})
-    localStorage.setItem('jwt', token)
+    // localStorage.setItem('jwt', token)
+    var date = new Date().getTime()
+    myLocalStorage.set('jwt', token, date+24*3600*1000) //设置1天的过期时间 单位ms
 }
 
 // 删除Token
@@ -28,4 +31,22 @@ export function setUser (user) {
 export function getUser () {
     // console.log(JSON.stringify(sessionStorage.getItem('user')))
     return sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')): null
+}
+
+const myLocalStorage = {
+    set: function (key, value, ttl_ms) {
+        var data = { value: value, expirse: new Date(ttl_ms).getTime() };
+        localStorage.setItem(key, JSON.stringify(data));
+    },
+    get: function (key) {
+        var data = JSON.parse(localStorage.getItem(key));
+        if (data !== null) {
+            if (data.expirse != null && data.expirse < new Date().getTime()) {
+                localStorage.removeItem(key);
+            } else {
+                return data.value;
+            }
+        }
+        return null;
+    }
 }
