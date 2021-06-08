42d7376989ca57cbe7c17eea3dd01f359a134e09
fzu221801127 <1647688598@qq.com>
2021-06-08 19:49:22 +0800

    fix: 添加注释

--- salthub/src/components/admin/notice/createnotice.vue
+++ salthub/src/components/admin/notice/createnotice.vue
@@ -1,3 +1,12 @@
+<!--
+ * @description:
+ * @fileName: createnotice.vue
+ * @author: 庄威龙
+ * @date: 2021-06-08 19:45:43
+ * @后台人员:
+ * @path:
+ * @version: V1.0.5
+!-->
 <template>
   <div class="columns">
     <div class="column is-full">
