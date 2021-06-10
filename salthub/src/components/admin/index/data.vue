53caeb0c40c765689bfc7177b49200fecf03858c
NoMornings <80037319+NoMornings@users.noreply.github.com>
2021-06-10 21:57:52 +0800

    fix: 管理端首页样式细化
    #56

--- salthub/src/components/admin/index/data.vue
+++ salthub/src/components/admin/index/data.vue
@@ -15,12 +15,56 @@
     </div>
     <div :class="'wrap'">
       <div :class="'rowcard'">
-        <div :class="'item'"></div>
-        <div :class="'item'"></div>
+        <div :class="'item'">
+          <div class="i-title">
+            <span>用户统计</span>
+            <el-tag size="mini">实时</el-tag>
+          </div>
+          <div class="i-data">
+            <span>{{ userNum }}</span>
+          </div>
+          <div class="i-detail">
+            <span>当前分类总记录数</span>
+          </div>
+        </div>
+        <div :class="'item'">
+          <div class="i-title">
+            <span>文章统计</span>
+            <el-tag type="warning" size="mini">实时</el-tag>
+          </div>
+          <div class="i-data">
+            <span>{{ userNum }}</span>
+          </div>
+          <div class="i-detail">
+            <span>当前分类总记录数</span>
+          </div>
+        </div>
       </div>
       <div :class="'rowcard'">
-        <div :class="'item'"></div>
-        <div :class="'item'"></div>
+        <div :class="'item'">
+          <div class="i-title">
+            <span>昨日新增用户数</span>
+            <el-tag type="success" size="mini">实时</el-tag>
+          </div>
+          <div class="i-data">
+            <span>{{ userNum }}</span>
+          </div>
+          <div class="i-detail">
+            <span>当前分类总记录数</span>
+          </div>
+        </div>
+        <div :class="'item'">
+          <div class="i-title">
+            <span>昨日新增文章数</span>
+            <el-tag type="danger" size="mini">实时</el-tag>
+          </div>
+          <div class="i-data">
+            <span>{{ userNum }}</span>
+          </div>
+          <div class="i-detail">
+            <span>当前分类总记录数</span>
+          </div>
+        </div>
       </div>
     </div>
   </el-card>
@@ -55,8 +99,33 @@ $grey: #dddddd;
     border-radius: 3px;
     background: $grey;
     width: 240px;
-    height: 60px;
-    display: inline-block;
+    height: auto;
+    font-size: 12px;
+    .i-title {
+      display: flex;
+      justify-content: space-between;
+      margin: 0 20px;
+    }
+    .i-data {
+      font-size: 20px;
+      font-weight: 500;
+      margin: 0 20px;
+    }
+    .i-detail {
+      display: flex;
+      justify-content: baseline;
+      margin: 0 20px;
+    }
   }
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      userNum: 1024,
+    };
+  },
+};
+</script>
