6e0decb00f3c2ef5d49ec37280fe1ea0246371e4
NoMornings <80037319+NoMornings@users.noreply.github.com>
2021-06-16 22:07:31 +0800

    feat: 官方文章管理接口

--- salthub/src/components/admin/ofBlog/table.vue
+++ salthub/src/components/admin/ofBlog/table.vue
@@ -38,22 +38,46 @@
         <el-table-column type="expand">
           <template slot-scope="props">
             <el-form label-position="left" inline class="demo-table-expand">
-              <el-form-item label="发表时间">
-                <span>{{ props.row.releaseTime }}</span>
-              </el-form-item>
-              <el-form-item label="管理员 ID">
+              <el-form-item label="博客 ID">
                 <span>{{ props.row.id }}</span>
               </el-form-item>
-              <el-form-item label="官方文章标题">
-                <span>{{ props.row.blog }}</span>
+              <el-form-item label="模块">
+                <span>
+                  <el-tag :type="moduleTag(props.row.module)">{{
+                    props.row.module
+                  }}</el-tag>
+                </span>
+              </el-form-item>
+              <el-form-item label="点赞">
+                <span>{{ props.row.likeNumber }}</span>
+              </el-form-item>
+              <el-form-item label="收藏">
+                <span>{{ props.row.collectionNumber }}</span>
+              </el-form-item>
+              <el-form-item label="内容">
+                <span>{{ props.row.content }}</span>
               </el-form-item>
             </el-form>
           </template>
         </el-table-column>
         <el-table-column label="发表时间" prop="releaseTime"> </el-table-column>
-        <el-table-column label="管理员 ID" prop="id"> </el-table-column>
-        <el-table-column label="官方文章标题" prop="blog" show-overflow-tooltip>
+        <el-table-column label="管理员 ID" prop="author"> </el-table-column>
+        <el-table-column label="官方文章标题" prop="title" show-overflow-tooltip>
         </el-table-column>
+        <el-table-column label="状态" prop="status" :filters="[
+            { text: '删除', value: '删除' },
+            { text: '封禁', value: '封禁' },
+            { text: '正常', value: '正常' },
+            { text: '树洞', value: '树洞' },
+          ]"
+          :filter-method="filterTag"
+          filter-placement="bottom-end"> 
+          <template slot-scope="scope">
+            <el-tag :type="statusTag(scope.row.status)">{{
+              scope.row.status
+            }}</el-tag>
+          </template>
+          </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button
@@ -75,8 +99,8 @@
     <el-col :span="21" :class="'center'">
       <div class="block p-center pagination">
         <el-pagination
-          layout="prev, pager, next"
-          :total="this.tableDataAll.length"
+          layout="total, prev, pager, next"
+          :total="total"
           @current-change="handleCurrentChange"
           :current-page="currentPage"
           :current-size="pageSize"
@@ -126,6 +150,7 @@
 </style>
 
 <script>
+import { getListByTag } from "@/api/postlist"
 export default {
   data() {
     return {
@@ -133,28 +158,16 @@ export default {
       tableData: [
         {
           releaseTime: "2021-4-28",
-          id: "王小虎",
-          blog: "福大计算机考研复试之人工智能分析篇",
-        },
-        {
-          releaseTime: "2021-4-28",
-          id: "王小虎",
-          blog: "2022福州大学考研指导来了，必看！！...",
-        },
-        {
-          releaseTime: "2021-4-28",
-          id: "王小虎",
-          blog: "考研改怎么准备呢",
-        },
-        {
-          releaseTime: "2021-4-28",
-          id: "王小虎",
-          blog: "考研每天应该学多久才能不成为炮灰？",
-        },
-        {
-          releaseTime: "2021-4-28",
-          id: "王小虎",
-          blog: "考研每天应该学多久才能不成为炮灰？",
+          // 用户ID
+          author: "烤盐人",
+          title: "福大计算机考研复试之人工智能分析篇",
+          status: "正常",
+          // 博客ID
+          id: 1,
+          module: "福州大学",
+          likeNumber: 0,
+          collectionNumber: 0,
+          content: "",
         },
       ],
       tableDataAll: [],
@@ -162,6 +175,9 @@ export default {
       currentPage: 1,
       pageSize: 10,
     };
+  },
+    mounted() {
+    this.init();
   },
   methods: {
     handleDetail(index, row) {
@@ -172,27 +188,135 @@ export default {
     },
     convert() {},
     handleCurrentChange: function (currentPage) {
-      console.log("handleCurrentChange()\n");
+      // console.log("handleCurrentChange()\n");
       this.tableData = [];
       this.currentPage = currentPage;
-      console.log("currentPage=" + currentPage + "\n");
-      var i;
-      for (
-        var i = (currentPage - 1) * 10, j = 0;
-        j < 10 &&
-        this.tableDataAll.length != 0 &&
-        i + j <= this.tableDataAll.length - 1;
-        j++, i++
-      ) {
-        console.log("i=" + i + "\n");
-        this.tableData.push(this.tableDataAll[i]);
-        console.log(
-          "this.tableDataAll[i]" + JSON.stringify(this.tableDataAll[i]) + "\n"
-        );
-      }
-      if (this.tableDataAll.length == 0) {
-        this.tableData = [];
-      }
+      // console.log("currentPage=" + currentPage + "\n");
+
+      // 分页获取博客列表
+      getAllBlog({
+        // 当前页码
+        current: this.currentPage,
+      }).then((response) => {
+        // console.log("blog=>", response.data.data.records);
+        var len = response.data.data.records.length;
+        var info = response.data.data.records;
+        for (var i = 0; i < len; i++) {
+          this.tableData.push({
+            releaseTime: "",
+            author: "",
+            title: "",
+            status: 2,
+            id: "",
+            module: 0,
+            likeNumber: "",
+            collectionNumber: "",
+            content: "",
+          });
+          // 发表时间
+          this.tableData[i].releaseTime = info[i].releaseTime;
+          // 管理员ID
+          this.tableData[i].author = info[i].author;
+          // 官方文章标题
+          this.tableData[i].title = info[i].title;
+          // 状态（ 删除 | 封禁 | 正常 | 树洞 ）
+          this.tableData[i].status = this.judgeStatus(info[i].state);
+          // 博客ID
+          this.tableData[i].id = info[i].id;
+          // 模块（ 福州大学 | 外校 | 杂谈 | 拼课 )
+          this.tableData[i].module = this.judgeModule(info[i].moduleId);
+          // 点赞数
+          this.tableData[i].likeNumber = info[i].likeNumber;
+          // 收藏数
+          this.tableData[i].collectionNumber = info[i].collectionNumber;
+          // 内容
+          this.tableData[i].content = info[i].content;
+        }
+        // this.tableData.pop();
+      });
+    },
+    // 判断模块类别
+    judgeModule(moduleId) {
+      if (moduleId == 0) return "福州大学";
+      else if (moduleId == 1) return "外校";
+      else if (moduleId == 2) return "杂谈";
+      else if (moduleId == 3) return "拼课";
+    },
+    // 根据模块类别渲染标签颜色
+    moduleTag(module) {
+      if (module == "福州大学") return "warning";
+      else if (module == "外校") return "success";
+      else if (module == "杂谈") return "info";
+      else return "";
+    },
+    // 判断博客状态status
+    judgeStatus(state) {
+      if (state == 0) return "删除";
+      else if (state == 1) return "封禁";
+      else if (state == 2) return "正常";
+      else if (state == 3) return "树洞";
+    },
+    // 根据用户状态渲染标签颜色
+    statusTag(status) {
+      if (status == "删除") return "info";
+      else if (status == "封禁") return "danger";
+      else if (status == "正常") return "success";
+      else if (status == "树洞") return "";
+    },
+    init() {
+      // 获取博客总数
+      // countBlog().then((response) => {
+        // this.total = response.data.data;
+        this.total = 3;
+        // console.log("countBlog()=>", response.data.data);
+      // });
+
+      // 分页获取博客列表
+      getListByTag(
+        // 当前页码
+        this.currentPage,
+        // tagId
+        2
+      ).then((response) => {
+        console.log("List=>", response.data.data.records);
+        var len = response.data.data.records.length;
+        var info = response.data.data.records;
+        for (var i = 0; i < len; i++) {
+          this.tableData.push({
+            releaseTime: "",
+            author: "",
+            title: "",
+            status: 2,
+            id: "",
+            module: 0,
+            likeNumber: "",
+            collectionNumber: "",
+            content: "",
+          });
+          // 发表时间
+          this.tableData[i].releaseTime = info[i].releaseTime;
+          // 管理员ID
+          this.tableData[i].author = info[i].author;
+          // 官方文章标题
+          this.tableData[i].title = info[i].title;
+          // 状态（ 删除 | 封禁 | 正常 | 树洞 ）
+          this.tableData[i].status = this.judgeStatus(info[i].state);
+          // 博客ID
+          this.tableData[i].id = info[i].id;
+          // 模块（ 福州大学 | 外校 | 杂谈 | 拼课 )
+          this.tableData[i].module = this.judgeModule(info[i].moduleId);
+          // 点赞数
+          this.tableData[i].likeNumber = info[i].likeNumber;
+          // 收藏数
+          this.tableData[i].collectionNumber = info[i].collectionNumber;
+          // 内容
+          this.tableData[i].content = info[i].content;
+        }
+        this.tableData.pop();
+      });
+    },
+    filterTag(value, row) {
+      return row.status === value;
     },
   },
 };
